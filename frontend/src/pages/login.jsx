cb3fe163230706f0c7408d2d3dd2e55e8b95e3cb
vrishabh081 <rishabhvishwakarma398@gmail.com>
2023-04-14 18:07:35 +0530

    added

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -16,11 +16,9 @@ const Login = () => {
         setLoader(true);
         axios.post("https://udemy-shubham-sahoo-server.vercel.app/login", payload)
         .then(res=>{
-            console.log(res);
             if(res.data.message){
                 setLoader(false);
                 alert(res.data.message);
-                localStorage.setItem("user", JSON.stringify(res.data))
                 navigate("/")
             }
             else{
@@ -28,7 +26,7 @@ const Login = () => {
             }
         })
         .catch(err=>{
-            console.log(err)
+            // console.log(err)
             setLoader(false);
         })
     }
